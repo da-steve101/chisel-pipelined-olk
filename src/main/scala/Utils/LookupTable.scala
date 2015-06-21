1a8e3e72f5fdcce4f4c3845ad7935faffca4a31a
Nick Fraser <icanlosh@gmail.com>
2015-06-21 14:10:11 +1000

    Added parameter and I/O information to LUT class.

--- src/main/scala/Utils/LookupTable.scala
+++ src/main/scala/Utils/LookupTable.scala
@@ -3,6 +3,14 @@ package OLK.Util
 import Chisel._
 import scala.collection.mutable.ArrayBuffer
 
+/** A basic, generic LUT implemented in chisel, with a.
+  * Parameters:
+  *     tableValues: ArrayBuffer[T], an array of values to define the LUT.
+  * Inputs:
+  *     addr: UInt, address to look up. We assume UInt < tableValues.length.
+  * Outputs:
+  *     out: T, The returned value of type T.
+  */
 class LookupTable[T <: Bits](val tableValues : ArrayBuffer[T]) extends Module {
   // table for generic type
   val io = new Bundle {
